850a3153bb0f6e59087858a17b75d040e6f2e83b
azimjonking <azimjonjalilov1301@mail.ru>
2022-11-28 20:34:41 +0200

    about page is started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Intro from "./components/intro/Intro";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div>
+      <Intro />
     </div>
   );
 }
