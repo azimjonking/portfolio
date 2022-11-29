246dffece10977f69d838bab92487b87d7aa2e48
azimjonking <azimjonjalilov1301@mail.ru>
2022-11-29 15:12:40 +0200

    contact form is started

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,59 @@
 import React from 'react';
 import "./contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
 
 const Contact = () => {
-  return (
-    <div className='c'>
-        contact
-    </div>
-  )
+    return (
+        <div className='c'>
+            <div className="c-bg"></div>
+            <div className="c-wrapper">
+                <div className="c-left">
+                    <h1 className="c-title">Let's discuss your project</h1>
+                    <div className="c-info">
+                        <div className="c-info-item">
+                            <img
+                                src={Phone}
+                                alt=""
+                                className="c-icon"
+                            />
+                            +1 1234 55 66
+                        </div>
+                        <div className="c-info-item">
+                            <img
+                                src={Email}
+                                alt=""
+                                className="c-icon"
+                            />
+                            contact@lama.dev
+                        </div>
+                        <div className="c-info-item">
+                            <img
+                                src={Address}
+                                alt=""
+                                className="c-icon"
+                            />
+                            245 King Street, Touterie Victoria 8520 Australia
+                        </div>
+                    </div>
+                </div>
+                <div className="c-right">
+                    <p className="c-desc">
+                        <b>What’s your story?</b> Get in touch. Always available for
+                        freelancing if the right project comes along. me.
+                    </p>
+                    <form>
+                        <input type="text" placeholder='Name' name="user_name" />
+                        <input type="text" placeholder='Subject' name="user_subject" />
+                        <input type="text" placeholder='Email' name="user_email" />
+                        <textarea rows="5" placeholder='Message' name="message" />
+                        <button>Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Contact;
