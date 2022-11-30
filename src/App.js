54e99f665057dc16dcdef62c636e26a6a1fcd5d5
azimjonking <azimjonjalilov1301@mail.ru>
2022-11-30 12:42:17 +0200

    toggle is started

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Intro from "./components/intro/Intro";
 import ProductList from "./components/productList/ProductList";
+import Toggle from "./components/toggle/Toggle";
 
 function App() {
   return (
     <div>
+      <Toggle />
       <Intro />
       <About />
       <ProductList />
