bc62b2f6ff10a64ee504915ff3f36ad9815f35af
azimjonking <azimjonjalilov1301@mail.ru>
2022-11-30 12:25:14 +0200

    contact page is done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,30 @@ import "./contact.css";
 import Phone from "../../img/phone.png";
 import Email from "../../img/email.png";
 import Address from "../../img/address.png";
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+
+    const formRef = useRef();
+    const [done, setDone] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            'service_cnlze07',
+            'template_ch9jzci',
+            formRef.current,
+            '7Y0QxQwwq55YHq0jE')
+            .then((result) => {
+                console.log(result.text);
+                setDone(true)
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
+
     return (
         <div className='c'>
             <div className="c-bg"></div>
@@ -43,12 +65,13 @@ const Contact = () => {
                         <b>What’s your story?</b> Get in touch. Always available for
                         freelancing if the right project comes along. me.
                     </p>
-                    <form>
+                    <form ref={formRef} onSubmit={handleSubmit}>
                         <input type="text" placeholder='Name' name="user_name" />
                         <input type="text" placeholder='Subject' name="user_subject" />
                         <input type="text" placeholder='Email' name="user_email" />
                         <textarea rows="5" placeholder='Message' name="message" />
                         <button>Submit</button>
+                        {done && "Thank you..."}
                     </form>
                 </div>
             </div>
